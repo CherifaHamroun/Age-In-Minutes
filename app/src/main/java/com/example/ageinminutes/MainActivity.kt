5cc8f6c29e374a1dded9233877737a42bd78e8a5
CherifaHamroun <hc_hamroun@esi.dz>
2020-08-28 11:42:31 +0200

    Interface Completed

---
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ageinminutes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnDatePicker.setOnClickListener { Toast.makeText(this,"Button Works",Toast.LENGTH_LONG).show() }
+    }
+}
